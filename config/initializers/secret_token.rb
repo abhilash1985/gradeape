1363188877ca8344cfd7157195b6f9a977851600
idbygeorge <george.joeckel@usu.edu>
2013-09-09 11:27:40 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gradeape::Application.config.secret_key_base = '22c0f962c4f9416aab25ab9c5031f52a7d74ade8ed812f9ef3181fc86fbe2a81f685d7aebba6cb00c463c23a46feba21d9d3a9d5411f134d6b6fa3309cd3c17a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gradeape::Application.config.session_store :cookie_store, key: '_gradeape_session'
