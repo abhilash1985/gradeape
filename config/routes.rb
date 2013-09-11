1a1f734394ccee76d5e88b795d18bc3e6f602987
idbygeorge <george.joeckel@usu.edu>
2013-09-11 13:46:03 -0600

    Delete manually created index. Add index with command line generate controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Gradeape::Application.routes.draw do
+  get "default/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
